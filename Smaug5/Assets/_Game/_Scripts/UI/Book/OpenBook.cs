b0b8ac71e3151d997179dc0c6b92a470697fa527
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2024-02-21 16:58:25 -0300

    Progresso Livro III

--- Smaug5/Assets/_Game/_Scripts/UI/Book/OpenBook.cs
+++ Smaug5/Assets/_Game/_Scripts/UI/Book/OpenBook.cs
@@ -12,6 +12,9 @@ public class OpenBook : MonoBehaviour
 
     private bool _isOpenClicked;
 
+    private DateTime _startTime;
+    private DateTime _endTime;
+
     private void Start()
     {
         if (openBtn != null)
@@ -21,12 +24,21 @@ public class OpenBook : MonoBehaviour
     private void Update()
     {
         if (_isOpenClicked)
+        {
             transform.Rotate(_rotation * Time.deltaTime);
+            _endTime = DateTime.Now;
+
+            if ((_endTime - _startTime).TotalSeconds >= 1)
+                _isOpenClicked = false;
+        }
     }
 
     private void ClickOpen()
     {
         _isOpenClicked = true;
+        _startTime  = DateTime.Now;
         _rotation = new Vector3(0, 180, 0);
+
+        // TODO: SFX abrindo livro
     }
 }
