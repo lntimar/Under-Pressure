c34e3828f3740667c88436c58c82d99ffcbe3d55
Lucas Timar <101904786+lntimar@users.noreply.github.com>
2024-03-10 23:00:14 -0300

    Falhei miseravelmente, amanhã retornarei mais forte

---
+++ Smaug5/Assets/_Game/_Scripts/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
