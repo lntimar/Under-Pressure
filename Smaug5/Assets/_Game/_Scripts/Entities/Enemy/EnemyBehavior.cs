a8863d4c6c36e1d26990a31f60efc86ea7184712
Lucas Timar <101904786+lntimar@users.noreply.github.com>
2024-03-13 18:00:12 -0300

    Verificando erros

--- Smaug5/Assets/_Game/_Scripts/Entities/Enemy/EnemyBehavior.cs
+++ Smaug5/Assets/_Game/_Scripts/Entities/Enemy/EnemyBehavior.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public float lookRadius = 10f;
 
     void Start()
     {
-        
+       
     }
     
     void Update()
     {
         
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
