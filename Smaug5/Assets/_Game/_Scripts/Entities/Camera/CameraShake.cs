8885ba77a4ce0064963e6ad8d5f6b22d520a0b6f
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2024-04-15 20:52:19 -0300

    Screen Shake

---
+++ Smaug5/Assets/_Game/_Scripts/Entities/Camera/CameraShake.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    #region Variáveis Globais
+    [Header("Configurações:")]
+
+    [SerializeField] private float animationSpeed;
+
+    private Animator _camAnim;
+    #endregion
+
+    #region Funções Unity
+    private void Awake()
+    {
+        _camAnim = gameObject.GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        ApplyScreenShake();
+
+        _camAnim.speed = animationSpeed;
+
+        InvokeRepeating("ApplyScreenShake", 1f, 0.1f);
+    }
+    #endregion
+
+    #region Funções Próprias
+    public void ApplyScreenShake()
+    {
+        _camAnim.SetTrigger("shake");
+        _camAnim.SetInteger("random", Random.Range(1, 3));
+    }
+    #endregion
+}
