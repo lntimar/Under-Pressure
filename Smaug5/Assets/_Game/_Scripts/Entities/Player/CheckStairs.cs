c43f92e507de45648f2f7132c26a183914c2b082
Matheus Duca <63525232+m-duca@users.noreply.github.com>
2024-06-14 11:02:26 -0300

    Rotação Escada

--- Smaug5/Assets/_Game/_Scripts/Entities/Player/CheckStairs.cs
+++ Smaug5/Assets/_Game/_Scripts/Entities/Player/CheckStairs.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CheckStairs : MonoBehaviour
 {
+    // Referências:
     private PlayerMove _playerMoveScript;
 
     private void Awake() => _playerMoveScript = FindObjectOfType<PlayerMove>();
-
+    
     private void OnTriggerStay(Collider collision) => _playerMoveScript.HasTouchStairs = true;
     
     private void OnTriggerExit(Collider collision) => _playerMoveScript.HasTouchStairs = false;
