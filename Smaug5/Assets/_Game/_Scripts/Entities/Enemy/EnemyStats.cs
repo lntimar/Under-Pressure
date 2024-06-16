76f13cf45da20de74705bff6896b846cd1beb424
Lucas Timar <101904786+lntimar@users.noreply.github.com>
2024-06-15 21:29:11 -0300

    Dano inimigo

--- Smaug5/Assets/_Game/_Scripts/Entities/Enemy/EnemyStats.cs
+++ Smaug5/Assets/_Game/_Scripts/Entities/Enemy/EnemyStats.cs
@@ -11,7 +11,7 @@ public class EnemyStats : MonoBehaviour
     [Header("Estatísticas do Inimigo")]
     public int MaxHealth = 100;
     public int CurrentHealth;
-    public static int Damage = 20;
+    public static int Damage = 15;
     [SerializeField] private float destroyTime = 1.25f;
 
     [Header("Referências")]
