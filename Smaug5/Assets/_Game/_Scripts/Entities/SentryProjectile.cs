7f7f23fe6bd2981cd23a83c2884b030c367e7755
Reimyel <caiquegsambrana@gmail.com>
2024-10-17 11:06:02 -0300

    a

--- Smaug5/Assets/_Game/_Scripts/Entities/SentryProjectile.cs
+++ Smaug5/Assets/_Game/_Scripts/Entities/SentryProjectile.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class SentryProjectile : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public LayerMask wallAndGround;
+    public LayerMask player;
+    PlayerStats playerStats;
+
+    private void Update()
     {
-        if (other.gameObject.tag == "Player")
+        Destroy(gameObject, 5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == wallAndGround)
         {
             Destroy(gameObject);
         }
+
+        //lógica de colisão com jogador tá no PlayerCollision
+
+        /*if (collision.gameObject.layer == player)
+        {
+            PlayerStats playerStats = collision.gameObject.GetComponent<PlayerStats>();
+            if (playerStats != null)
+            {
+                playerStats.ChangeHealthPoints(-10);
+            }
+            Destroy(gameObject);
+        }*/
     }
 }
