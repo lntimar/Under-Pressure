a67881cdd19abc32ea614057d91aeb9f7b140274
Lucas Timar <101904786+lntimar@users.noreply.github.com>
2024-10-20 21:13:40 -0300

    creating enemy

--- Smaug5/Assets/_Game/_Scripts/Entities/IdleState.cs
+++ Smaug5/Assets/_Game/_Scripts/Entities/IdleState.cs
@@ -14,7 +14,7 @@ public class IdleState : StateMachineBehaviour
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log(timer);
+        //Debug.Log(timer);
         timer += Time.deltaTime;
         if (timer >= 5)
             animator.SetBool("isPatrolling", true);
